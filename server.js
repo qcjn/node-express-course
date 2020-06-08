4bb2ffb93fd8f04304b6f30a8ef3799f113e3ec1
qcjn <2111985819@qq.com>
2020-06-08 09:08:40 +0800

    add get route

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ app.get("/", (req, res) => {
 	res.send("Hello, World!")
 })
 
+const mockUserData = [ 
+				{name: 'Mark'},
+				{name: 'Jill'}
+			]
+			
+app.get("/users", (req, res) => {
+	res.json({
+		success: true,
+		message: 'successfully got users, Nice!',
+		users: mockUserData
+	})
+})
+
 app.listen(8000, () => {
 	console.log("Server is running...")
 })
